6e6b4e00a31c2a17359b2108e4556b3c1cc079ed
Nelli Sviili <nelli.sviili@gmail.com>
2023-12-30 13:18:34 +0100

    teht 4.4 step2 valmis

--- blogilista/tests/dummy.test.js
+++ blogilista/tests/dummy.test.js
@@ -1,10 +1,97 @@
-// teht 4.3 teht 4.3 apufunktioita ja yksikkötestejä step1
-// dummy funktion testaus
+// teht 4.4 apufunktioita ja yksikkötestejä step2
+// testataan totalLikes, joka laskee taulukoiden tykkäykset yhteen
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+})
+
+describe('totalLikes', () => {
+  test('empty list is zero', () => {
+    const blogs = []
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+})
+
+describe('total likes with one blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+})
+
+describe('total likes with many blogs', () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+
+  test('when list has many blogs', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(36)
+  })
 })
--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -1,10 +1,17 @@
-// teht 4.3 teht 4.3 apufunktioita ja yksikkötestejä step1 OK
-// funktio dummy, joka saa taulukollisen blogeja ja palauttaa aina nro 1 
+// teht 4.4 apufunktioita ja yksikkötestejä step2 OK
+// funktio totalLikes laskee liket yhteen
+
 const dummy = (blogs) => {
     // ...saa taulukollisen blogeja ja palauttaa luvun 1
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+  // saa taulukollisen blogeja ja palauttaa blogien yht. tykkäysten määrän
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
