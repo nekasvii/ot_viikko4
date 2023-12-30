28d6d3737e62ab8b8700b178f8d9eb219fa57f61
Nelli Sviili <nelli.sviili@gmail.com>
2023-12-30 14:20:20 +0100

    teht 4.6 step4 valmis

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -1,5 +1,5 @@
-// teht 4.5 apufunktioita ja yksikkötestejä step3 OK
-// favoriteBlog-funktio selvittää blogin, jolla eniten tykkäyksiä
+// teht 4.6 apufunktioita ja yksikkötestejä step4 OK
+// mostBlogs-funktio, joka selvittää kirjoittajan, jolla on eniten blogeja
 
 const dummy = (blogs) => {
     // ...saa taulukollisen blogeja ja palauttaa luvun 1
@@ -27,9 +27,37 @@ const favoriteBlog = (blogs) => {
     likes: favorite.likes
   }
 }
+
+const mostBlogs = (blogs) => {
+  // saa taulukon blogeja ja palauttaa kirjoittajan, jolla on eniten blogeja ja blogien lkm
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  const authorCounter = blogs.reduce((counts, blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1
+    return counts;
+  }, {})
+
+  let maxBlogs = 0
+  let busiestAuthor = ''
+
+  for (const author in authorCounter) {
+    if (authorCounter[author] > maxBlogs) {
+      maxBlogs = authorCounter[author]
+      busiestAuthor = author
+    }
+  }
+  
+  return {
+    author: busiestAuthor,
+    blogs: maxBlogs
+  }
+}
   
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
